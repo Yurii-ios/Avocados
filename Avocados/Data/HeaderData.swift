b9a833da0a25917e2e90e96d9b45e8a9cc496c76
Yurii <Yurii>
2021-01-10 22:44:48 +0100

    headers data

---
+++ Avocados/Data/HeaderData.swift
@@ -0,0 +1,43 @@
+//
+//  HeaderData.swift
+//  Avocados
+//
+//  Created by Yurii Sameliuk on 10/01/2021.
+//
+
+import SwiftUI
+
+//MARK: - HEADER DATA
+
+let headersData: [HeaderModel] = [
+    HeaderModel(
+        image: "avocado-slice-1",
+        headline: "Avocados",
+        subheadline: "Avocados are a powerhouse ingredient at any meal for anyone."
+    ),
+    HeaderModel(
+        image: "avocado-slice-2",
+        headline: "Healthy",
+        subheadline: "Avocados are good for us and they can be part of our healthy diet."
+    ),
+    HeaderModel(
+        image: "avocado-slice-3",
+        headline: "Nutrients",
+        subheadline: "Avocados have a surprising amount and diversity of vitamins and minerals."
+    ),
+    HeaderModel(
+        image: "avocado-slice-4",
+        headline: "Delicious",
+        subheadline: "Craving more guacamole? Find the best guacamole recipes in the app."
+    ),
+    HeaderModel(
+        image: "avocado-slice-5",
+        headline: "Tasty",
+        subheadline: "Avocados are a good source of natural fiber, which making you feel full."
+    ),
+    HeaderModel(
+        image: "avocado-slice-6",
+        headline: "Recipes",
+        subheadline: "Enjoy these carefully handpicked avocado recipes for every occasion!"
+    )
+]
