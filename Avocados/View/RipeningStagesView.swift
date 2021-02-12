e6398d7302a14893da17ff23d8f28447ef5caea7
Yurii <Yurii>
2021-02-12 18:56:44 +0100

    completed work with SettingsView

--- Avocados/View/RipeningStagesView.swift
+++ Avocados/View/RipeningStagesView.swift
@@ -9,16 +9,31 @@ import SwiftUI
 
 struct RipeningStagesView: View {
     //MARK: - PROPERTIES
+    var ripeningStages: [RipeningModel] = ripeningData
     
     //MARK: - BODY
     var body: some View {
-        Text("Ripening Stages")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages) { item in
+                        RipeningView(ripening: item)
+                    }
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                Spacer()
+            }
+            
+        } //: SCROLL
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 //MARK: - PREVIEW
 struct RipeningStagesView_Previews: PreviewProvider {
     static var previews: some View {
-        RipeningStagesView()
+        RipeningStagesView(ripeningStages: ripeningData)
     }
 }
